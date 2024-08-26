fa6da06cbc208b45f28920b3ac98116f49bf548f
HussainDEV10 <trendshussain@gmail.com>
2024-08-26 15:58:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ loginForm.addEventListener('submit', async (e) => {
 
     try {
         await auth.signInWithEmailAndPassword(email, password);
-        window.location.href = 'https://hussaindev10.github.io/posts/?';
+        window.location.href = 'https://hussaindev10.github.io/posts/';
     } catch (error) {
         alert('خطأ في تسجيل الدخول: ' + error.message);
     }
@@ -35,7 +35,7 @@ signupLink.addEventListener('click', () => {
     
     auth.createUserWithEmailAndPassword(email, password)
         .then(() => {
-            window.location.href = 'https://hussaindev10.github.io/posts/?';
+            window.location.href = 'https://hussaindev10.github.io/posts/';
         })
         .catch((error) => {
             alert('خطأ في إنشاء الحساب: ' + error.message);
