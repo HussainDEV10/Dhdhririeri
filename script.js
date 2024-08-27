c2beabbee09affb3940ab870e3c167d4e698b1f6
HussainDEV10 <trendshussain@gmail.com>
2024-08-27 18:09:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ loginBtn.addEventListener('click', async () => {
             localStorage.setItem('username', username);
 
             // التبديل إلى صفحة المنشورات
-            window.location.href = 'https://hussaindev10.github.io/Mon/';
+            window.location.href = 'https://hussaindev10.github.io/Mon/?';
         } catch (error) {
             errorMessage.textContent = `خطأ في تسجيل الدخول: ${error.message}`;
         }
@@ -67,7 +67,7 @@ signupBtn.addEventListener('click', async () => {
             localStorage.setItem('username', username);
 
             // التبديل إلى صفحة المنشورات
-            window.location.href = 'https://hussaindev10.github.io/Mon/';
+            window.location.href = 'https://hussaindev10.github.io/Mon/?';
         } catch (error) {
             errorMessage.textContent = `خطأ في إنشاء الحساب: ${error.message}`;
         }
